b26905e625f3051a6f3b9d29ab797e35e35aeb54
DanilaMust <dkressa01@mail.ru>
2024-01-16 14:21:52 +0600

    react-counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.scss';
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
+
+  const [state, setState] = React.useState(0);
+
+  const onClickPlus = () => {
+    setState(state+1)
+  }
+
+  const onClickMinus = () => {
+    if (state >= 1) {
+      setState(state-1)
+    }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>{state}</h1>
+        <button onClick={onClickMinus} className="minus">- Минус</button>
+        <button onClick={onClickPlus} className="plus">Плюс +</button>
+      </div>
     </div>
   );
 }
